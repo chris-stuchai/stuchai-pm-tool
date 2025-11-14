e4946c813a2276addefd66b477f44fb219862d44
Chris Hernandez <support@stuchai.com>
2025-11-13 21:52:54 -0600

    Fix missing imports in DashboardLayout

--- components/layout/DashboardLayout.tsx
+++ components/layout/DashboardLayout.tsx
@@ -1,7 +1,9 @@
 "use client"
 
+import Link from "next/link"
 import { Sidebar } from "./Sidebar"
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Button } from "@/components/ui/button"
 import {
   DropdownMenu,
   DropdownMenuContent,
