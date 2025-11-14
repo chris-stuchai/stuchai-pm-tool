0cb2668efeaf310d5d6a491df7c45d65ce8c2284
Chris Hernandez <support@stuchai.com>
2025-11-14 14:58:09 -0600

    Fix middleware blocking public assets (logo.png)
    - Update middleware matcher to exclude common image and file extensions
    - Allow public files (.png, .jpg, .gif, .svg, .txt, etc.) to be served without auth
    - This fixes the logo not loading because it was being redirected to sign-in

--- middleware.ts
+++ middleware.ts
@@ -14,7 +14,16 @@ export default withAuth(
 
 export const config = {
   matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|auth).*)",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - auth (auth pages)
+     * - public files (images, txt, etc.)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|auth|.*\\.png$|.*\\.jpg$|.*\\.jpeg$|.*\\.gif$|.*\\.svg$|.*\\.ico$|.*\\.txt$).*)",
   ],
 }
 
